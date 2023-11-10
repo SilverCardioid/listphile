8a6df710f9daea7f5b523a31a6e43f9cf1e67645
SilverCardioid <sibridband@gmail.com>
2023-11-11 00:54:37 +0100

    Compare updates: ListItems, sort key for names, logic fix

--- listphile/compare.py
+++ listphile/compare.py
@@ -2,8 +2,9 @@ import os
 from pathlib import Path
 import typing as ty
 
-from . import format, filelist, paths
 from . import config_helpers as ch
+from . import format, filelist, paths
+from .filelist import ListItem
 
 class DiffItem(ty.NamedTuple):
 	diff_type:str
@@ -18,78 +19,83 @@ class FileListComparer(filelist.FileLister):
 	            ) -> ty.Generator[DiffItem, None, None]:
 		old_gen = self._get_gen(old_list)
 		new_gen = self._get_gen(new_list)
+		empty_item = ListItem(None, None, None)
 
-		old_type, old_path, old_props = next(old_gen, (None, None, None))
-		new_type, new_path, new_props = next(new_gen, (None, None, None))
+		old = next(old_gen, empty_item)
+		new = next(new_gen, empty_item)
 
-		while old_type is not None or new_type is not None:
-			diff = self._compare_lines(old_type, old_props, new_type, new_props)
+		while old.item_type is not None or new.item_type is not None:
+			diff = self._compare_lines(old, new)
 			if diff == 0:
-				assert old_path == new_path
-				if (old_type == 'file' and not names_only and
-				    self._compare_files(old_props, new_props)):
-					yield DiffItem('change', new_type, new_path, old_props, new_props)
+				assert old.path == new.path
+				if (old.item_type == 'file' and not names_only and
+				    not self._compare_files(old.props, new.props)):
+					yield DiffItem('change', new.item_type, new.path, old.props, new.props)
 				else:
-					yield DiffItem('match', new_type, new_path, old_props, new_props)
-				old_type, old_path, old_props = next(old_gen, (None, None, None))
-				new_type, new_path, new_props = next(new_gen, (None, None, None))
+					yield DiffItem('match', new.item_type, new.path, old.props, new.props)
+				old = next(old_gen, empty_item)
+				new = next(new_gen, empty_item)
 
 			elif diff < 0:
-				yield DiffItem('deletion', old_type, old_path, old_props, None)
+				yield DiffItem('deletion', old.item_type, old.path, old.props, None)
 
 				can_skip_children = False
-				if skip_children and new_type == 'dir':
-					old_dir_depth = self._get_depth(old_props)
+				if skip_children and old.item_type == 'dir':
+					old_dir_depth = self._get_depth(old.props)
 					if old_dir_depth is not None:
 						can_skip_children = True
-				old_type, old_path, old_props = next(old_gen, (None, None, None))
-				while can_skip_children and old_props and self._get_depth(old_props) > old_dir_depth:
-					old_type, old_path, old_props = next(old_gen, (None, None, None))
+				old = next(old_gen, empty_item)
+				while can_skip_children and old.props and self._get_depth(old.props) > old_dir_depth:
+					old = next(old_gen, empty_item)
 
 			else:
-				yield DiffItem('addition', new_type, new_path, None, new_props)
+				yield DiffItem('addition', new.item_type, new.path, None, new.props)
 
 				can_skip_children = False
-				if skip_children and new_type == 'dir':
-					new_dir_depth = self._get_depth(new_props)
+				if skip_children and new.item_type == 'dir':
+					new_dir_depth = self._get_depth(new.props)
 					if new_dir_depth is not None:
 						can_skip_children = True
-				new_type, new_path, new_props = next(new_gen, (None, None, None))
-				while can_skip_children and new_props and self._get_depth(new_props) > new_dir_depth:
-					new_type, new_path, new_props = next(new_gen, (None, None, None))
+				new = next(new_gen, empty_item)
+				while can_skip_children and new.props and self._get_depth(new.props) > new_dir_depth:
+					new = next(new_gen, empty_item)
 
 	def _get_depth(self, props:format.Props) -> ty.Optional[int]:
 		return props.get_depth(start_level=self.options.start_level, indent=self.options.indent)
 
-	def _compare_lines(self, old_type:ty.Optional[str], old_props:format.Props,
-	                         new_type:ty.Optional[str], new_props:format.Props) -> int:
+	def _compare_lines(self, old:ListItem, new:ListItem) -> int:
 		# positive result -> addition (old list ahead, new line missing in old list)
 		# list that hasn't run out yet -> missing in other list
-		if old_type is None or new_type is None:
-			return (old_type is None) - (new_type is None)
+		if old.item_type is None or new.item_type is None:
+			return (old.item_type is None) - (new.item_type is None)
 		# higher indent -> missing in other list
-		old_depth, new_depth = self._get_depth(old_props), self._get_depth(new_props)
+		old_depth, new_depth = self._get_depth(old.props), self._get_depth(new.props)
 		if old_depth is not None and new_depth is not None:
 			if old_depth != new_depth:
 				return -old_depth + new_depth
-			if (old_type == 'dir' and new_type == 'dir' and
+			if (old.item_type == 'dir' and new.item_type == 'dir' and
 			    old_depth == new_depth == 0):
 				# root folders always match
 				return 0
 		# child file before child folder (or vice versa) -> missing in other list
+		old_isdir, new_isdir = 'dir' in old.item_type, 'dir' in new.item_type
 		item_grouping = ch._get_enum(ch.GroupType, self.options.item_grouping)
 		if item_grouping != ch.GroupType.MIXED:
 			if item_grouping == ch.GroupType.FILESFIRST:
-				isdir_diff = (old_type == 'dir') - (new_type == 'dir')
+				isdir_diff = old_isdir - new_isdir
 			else:
-				isdir_diff = -(old_type == 'dir') + (new_type == 'dir')
+				isdir_diff = -old_isdir + new_isdir
 			if isdir_diff != 0:
 				return isdir_diff
 		# alphabetically first -> missing in other list
-		old_name = old_props.get_name()
-		new_name = new_props.get_name()
+		old_name = old.props.get_name()
+		new_name = new.props.get_name()
 		if old_name is not None and new_name is not None:
-			return (old_name > new_name) - (old_name < new_name)
+			if self.options.sort_key:
+				# todo: base folder?
+				old_name = self.options.sort_key(paths.PathItem(None, old.path, old_depth or 0, old_isdir))
+				new_name = self.options.sort_key(paths.PathItem(None, new.path, new_depth or 0, new_isdir))
+			return (new_name < old_name) - (old_name < new_name)
 		return 0
 
 
@@ -106,7 +112,7 @@ class FileListComparer(filelist.FileLister):
 		return True
 
 	def _get_gen(self, list_or_folder:paths.PathOrStr
-	             ) -> ty.Generator[ty.Tuple[str, Path, format.Props], None, None]:
+	             ) -> ty.Generator[ListItem, None, None]:
 		if os.path.isdir(list_or_folder):
 			return self.generate(list_or_folder)
 		elif os.path.isfile(list_or_folder):
